439711b1b85408cebb95db460c44d1382f59403d
SARANGATHALAGALAGE <140165039+SARANGATHALAGALAGE@users.noreply.github.com>
2024-07-15 11:47:00 +0530

    Update q1.scala

--- q1.scala
+++ q1.scala
@@ -1,8 +1,7 @@
 import scala.io.StdIn.readLine
 
 object InventoryManagementSystem {
-
-  // Function to get product list from user input
+  
   def getProductList(): List[String] = {
     var products = List[String]()
     var input = ""
@@ -17,19 +16,16 @@ object InventoryManagementSystem {
     products
   }
 
-  // Function to print product list with their positions
   def printProductList(products: List[String]): Unit = {
     products.zipWithIndex.foreach { case (product, index) =>
       println(s"${index + 1}. $product")
     }
   }
 
-  // Function to get the total number of products
   def getTotalProducts(products: List[String]): Int = {
     products.length
   }
 
-  // Main function to demonstrate the functionality
   def main(args: Array[String]): Unit = {
     val productList = getProductList()
     println("\nProduct List:")
